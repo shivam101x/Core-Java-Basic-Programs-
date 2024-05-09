16f2400c8d6edf583e58e6d6f0700b919ac22c9f
Shivam <malvadeshivam@gmail.com>
2024-05-09 11:29:26 +0530

    Create age.java

---
+++ age.java
@@ -0,0 +1,12 @@
+public class age {
+		  public static void main(String[] args) {   
+		    int age = 20;
+		    String result;
+		    result = (age > 18) ? "ready to vote." : "not ready to vote.";
+		    System.out.println(result);
+		  }
+		}
+
+
+
+	
