814b964e852bbc587ceca16a211309fadb6b0910
Shivam <malvadeshivam@gmail.com>
2024-05-15 11:26:02 +0530

    Create While.java

---
+++ While.java
@@ -0,0 +1,20 @@
+
+public class While {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a[]= {10,3,9,15,89,77,20,30,40,50};
+		int i=0;
+		while (i<a.length) {
+			int num=a[i];
+			if(num%2==0);
+			{
+				System.out.println(num);
+			}
+			i++;
+		}
+		
+	}
+
+}
