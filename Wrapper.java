d905ecc7495de422603686ae69e9d585e4b05b73
Shivam 💫 <133480835+shivam101x@users.noreply.github.com>
2024-05-21 10:48:04 +0530

    Add files via upload

---
+++ Wrapper.java
@@ -0,0 +1,15 @@
+
+public class Wrapper {
+
+	public static void main(String[] args) {
+		int a=10;
+		Integer ab=a; //autoboxing   primitive to wrapper
+		
+		Integer in=200;
+		int xy=in;  // Unboxing  wrapper to primitive
+		
+		System.out.println(ab);
+		System.out.println(xy);
+	}
+
+}
